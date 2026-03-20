291a1cf6dc2d4a6d3a54b1ad9a63a98dd6146141
Not Flopgop <137199787+Flapgop@users.noreply.github.com>
2026-03-20 08:26:17 -0700

    Fix swerve configuration

--- src/main/java/frc/robot/sultans3965/Robot.java
+++ src/main/java/frc/robot/sultans3965/Robot.java
@@ -8,8 +8,6 @@ package frc.robot.sultans3965;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.sultans3965.subsystems.SwerveSubsystem;
-
 
 public class Robot extends TimedRobot
 {
